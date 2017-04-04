40bb79154e1d03a9e1a8641f4591f09a608bcec2
KGP <kgp1202@gmail.com>
2017-04-04 16:06:14 +0900

    insert complete

--- ConsoleApplication1/ConsoleApplication1/소스.c
+++ ConsoleApplication1/ConsoleApplication1/소스.c
@@ -30,22 +30,22 @@ Node* init(int max_size) {
 Node* initNode() {
 	Node* retNode = (Node*)malloc(sizeof(Node));
 
-//	memset(retNode->key, MAX_VALUE, sizeof(KEY_TYPE) * CHILD_SIZE);
 	for (int i = 0; i < CHILD_SIZE; i++) {
 		retNode->key[i] = MAX_VALUE;
 	}
 	retNode->isLeaf = 0;
+	retNode->parent = NULL;
 
 	return retNode;
 }
 Node* initLeafNode() {
 	Node* retNode = (Node*)malloc(sizeof(Node));
 	
-	//memset(&retNode->key[0], MAX_VALUE, sizeof(KEY_TYPE) * (CHILD_SIZE));
 	for (int i = 0; i < CHILD_SIZE; i++) {
 		retNode->key[i] = MAX_VALUE;
 	}
 	retNode->isLeaf = 1;
+	retNode->parent = NULL;
 
 	return retNode;
 }
@@ -53,36 +53,58 @@ Node* initLeafNode() {
 KEY_TYPE InsertTempKey[CHILD_SIZE];
 void* InsertTempValue[CHILD_SIZE];
 
-void Insert(Node* root, KEY_TYPE inputKey, void* inputValue) {
+//Root Node에 대한 설정 수정 요망@@@@@@@@@@@@@@@@
+Node* root;
+void Insert(Node* root2, KEY_TYPE inputKey, void* inputValue) {
 	Node* current = root;
 	char insertComplete = 0;
 	char doingSpilt = 0;
 	while (insertComplete == 0) {
 		for (int i = 0; i < CHILD_SIZE; i++) {
-			if (current->key[i] >= inputKey) {
+			if (current->key[i] > inputKey) {
 				if(!(current->isLeaf) && !doingSpilt)	//Not LeafNode
 				{	
 					current = ((Node**)current->child)[i];
+					break;
 				}
 				else {								//LeafNode
-					//Move Back Key & Child
-					memcpy(InsertTempKey, &(current->key[i]), sizeof(KEY_TYPE) * (CHILD_SIZE - i - 1));
-					memcpy(InsertTempValue, &((Node**)current->child)[i], sizeof(void*) * (CHILD_SIZE - i - 1));
-					if (i + 1 < CHILD_SIZE) {
-						memcpy(&(current->key[i + 1]), InsertTempKey, sizeof(KEY_TYPE) * (CHILD_SIZE - i - 1));
-						memcpy(&((Node**)current->child)[i + 1], InsertTempValue, sizeof(void*) * (CHILD_SIZE - i - 1));
+					//Move Back Key
+					int howManyMove = CHILD_SIZE - i - 1;
+					if (howManyMove > 0) {
+						memcpy(InsertTempKey, &(current->key[i]), sizeof(KEY_TYPE) * howManyMove);
+						memcpy(&(current->key[i + 1]), InsertTempKey, sizeof(KEY_TYPE) * howManyMove);
 					}
 					current->key[i] = inputKey;
-					current->child[i] = inputValue;
+
+					//Move Back Child
+					if (current->isLeaf) {
+						if (howManyMove > 0) {
+							memcpy(InsertTempValue, &((Node**)current->child)[i], sizeof(void*) * howManyMove);
+							memcpy(&((Node**)current->child)[i + 1], InsertTempValue, sizeof(void*) * howManyMove);
+						}
+						current->child[i] = inputValue;
+					}
+					else {
+						if (howManyMove > 0) {
+							memcpy(InsertTempValue, &((Node**)current->child)[i + 1], sizeof(void*) * howManyMove);
+							memcpy(&((Node**)current->child)[i + 2], InsertTempValue, sizeof(void*) * howManyMove);
+						}
+						current->child[i + 1] = inputValue;
+
+						((Node*)inputValue)->parent = current;
+					}
+					
 					
 					//Spilt Check
 					if (current->key[CHILD_SIZE - 1] != MAX_VALUE) {
 						doingSpilt = 1;
+						inputKey = current->key[CHILD_SIZE / 2];
 
+						//set InputValue
 						if (current->isLeaf) {		
 							//create LeafNode
 							inputValue = (void*)initLeafNode();
-							
+
 							//connect each other 
 							((Node*)inputValue)->child[CHILD_SIZE] = current->child[CHILD_SIZE];
 							current->child[CHILD_SIZE] = inputValue;
@@ -92,7 +114,9 @@ void Insert(Node* root, KEY_TYPE inputKey, void* inputValue) {
 							int howManyCopy = CHILD_SIZE - CHILD_SIZE / 2;
 							memcpy(((Node*)inputValue)->child, &current->child[copyStart], sizeof(void*) * howManyCopy);
 							memcpy(((Node*)inputValue)->key, &current->key[copyStart], sizeof(KEY_TYPE) * howManyCopy);
-							memset(&current->child[copyStart], MAX_VALUE, sizeof(KEY_TYPE) * howManyCopy);
+							for (int t = copyStart; t < CHILD_SIZE; t++) {
+								current->key[t] = MAX_VALUE;
+							}
 						}
 						else {
 							//create Node
@@ -100,33 +124,32 @@ void Insert(Node* root, KEY_TYPE inputKey, void* inputValue) {
 
 							//copy to inputValue
 							int copyStart = CHILD_SIZE / 2 + 1;
-							int keyHowManyCopy = CHILD_SIZE - CHILD_SIZE / 2;
-							int valueHowManyCopy = keyHowManyCopy + 1;
+							int valueHowManyCopy = CHILD_SIZE - CHILD_SIZE / 2;
+							int keyHowManyCopy = valueHowManyCopy - 1;
 							memcpy(((Node*)inputValue)->child, &current->child[copyStart], sizeof(void*) * valueHowManyCopy);
 							memcpy(((Node*)inputValue)->key, &current->key[copyStart], sizeof(KEY_TYPE) * keyHowManyCopy);
-							memset(&current->child[copyStart], MAX_VALUE, sizeof(KEY_TYPE) * keyHowManyCopy);
-
+							for (int t = copyStart - 1; t < CHILD_SIZE; t++) {
+								current->key[t] = MAX_VALUE;
+							}
+							
 							//set child's parent
 							int c = 0;
 							while (((Node*)inputValue)->key[c] != MAX_VALUE) {
 								((Node*)((Node*)inputValue)->child[c++])->parent = ((Node*)inputValue);
 							}
-						}
-						
-						inputKey = current->key[CHILD_SIZE / 2];
+						}	
 						
 						//root expension
 						if (current->parent == NULL) {
-
-						fefefe]
-						else {
-									
+							current->parent = initNode();
+							root = current->parent;
+							root->child[0] = current;
 						}
+						current = (Node*)current->parent;
 					}
 					else {
 						insertComplete = 1;
 					}
-
 					break;
 				}
 			}
@@ -146,6 +169,7 @@ void Search() {
 void print(Node* root) {
 	if (root->isLeaf) {
 		printf("Leaf Node\n");
+		//printf("parent is %d\n", ((Node*)root->parent)->key[0]);
 	}
 	else {
 		printf("Directory Node\n");
@@ -163,8 +187,9 @@ void print(Node* root) {
 	if (root->isLeaf) {
 		return;
 	}
-	for (int i = 0; i < CHILD_SIZE - 1; i++) {
+	for (int i = 0; i < CHILD_SIZE ; i++) {
 		if (root->key[i] == MAX_VALUE) {
+			print((Node*)root->child[i]);
 			return;
 		}
 		else {
@@ -174,10 +199,10 @@ void print(Node* root) {
 }
 
 int main() {
-	Node* root = initLeafNode();
+	root = initLeafNode();
 	root->parent = NULL;
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 10; i++) {
 		Data* d = (Data*)malloc(sizeof(Data));
 		d->testN = i;
 
